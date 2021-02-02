d5d79dd4c51967c85a9b71ec8004126e1d4e99e4
vaishali161197 <vaishali161197vs@gmail.com>
2021-02-02 18:17:59 +0530

    Text_Project

---
+++ Assets/script/AdventureGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponenet;
+    [SerializeField] State startingState;
+
+    
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponenet.text = state.GetStateStory();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for(int index =0; index < nextStates.Length; index ++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1+index))
+            {
+                state = nextStates[index];
+            }
+        }
+        textComponenet.text = state.GetStateStory();
+    }
+}
---
+++ Assets/script/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")]
+public class State : ScriptableObject
+{
+  [TextArea(20, 34) ] [SerializeField] string storytext;
+    [SerializeField] State[] nextStates;
+    public string GetStateStory()
+    {
+        return storytext;
+    }
+    public State [] GetNextStates()
+    {
+        return nextStates;
+    }
+}
